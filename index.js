55a86db93462c10d3aadb40ee34da82f75ec7cd8
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-04-01 15:51:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file.
+function lowerCaseDrivers(arr) {
+  return arr.map(obj => obj.toLowerCase());
+}
+
+function nameToAttributes(arr) {
+  return arr.map(obj => {
+    var rObj = {};
+    let splitted = obj.split(" ")
+    rObj['firstName'] = splitted[0];
+    rObj['lastName'] = splitted[1];
+    return rObj;
+  })
+}
+
+function attributesToPhrase(arr) {
+  return arr.map(obj => {
+    const sentence = obj["name"] + ' is from ' + obj["hometown"]
+    return sentence
+  })
+}
